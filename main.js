126e9e3e5545b90f69f13c30d5a27a82d2f3cb4c
github18122021 <vrajbagadiya0071@gmail.com>
2023-11-06 15:36:38 -0500

    Added Event Listeners.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let grabData = async function() {
     try {
         let resp = await fetch("http://localhost:5000/quotes");
         let data = await resp.json();
-        console.log(data);
+        // console.log(data);
         useData(data);
     } catch (error) {
         console.log(error.message);
@@ -18,8 +18,27 @@ let previous = document.querySelector(".previous");
 let quote = document.querySelector(".quote");
 let next = document.querySelector(".next");
 
+let index = 0;
+
 let useData= function(data) {
-    let index = 0;
-    let elm = data[index];
-    quote.textContent = elm.text;
+    quote.textContent = data[index].text;
+
+    previous.addEventListener("click", (e)=> {
+        if (index === 0) {
+            index = data.length - 1;
+        } else {
+            index = index - 1;
+        }
+        quote.textContent = data[index].text;
+    })
+    
+    next.addEventListener("click", (e)=> {
+        if (index === (data.length - 1)) {
+            index = 0;
+        } else {
+            index = index + 1;
+        }
+        quote.textContent = data[index].text;
+    })
+
 }
