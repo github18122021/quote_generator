38d38980d3b531e8ef18cd993f645b50b8e588cd
github18122021 <vrajbagadiya0071@gmail.com>
2023-11-06 14:50:12 -0500

    Fetched data from db.json

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", (e) => {
+    grabData();
+})
+
+
+let grabData = async function() {
+    try {
+        let resp = await fetch("http://localhost:5000/quotes");
+        let data = await resp.json();
+        console.log(data);
+        useData(data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+let previous = document.querySelector(".previous");
+let quote = document.querySelector(".quote");
+let next = document.querySelector(".next");
+
